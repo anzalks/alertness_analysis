2ea36d6e1b49ecb274b1f505614c20b8d6e9d9ca
Anzal KS <anzal.ks@gmail.com>
2018-12-18 00:06:02 +0530

    modified some ploting stuff

--- encoder calibration.py
+++ encoder calibration.py
@@ -13,18 +13,10 @@ os.chdir("/home/anzalks/DATA/Calibration_data/Calibration_data_M_1/2018-12-14T17
 hn=["timestamp_arduino", "trial_count_", "puff", "tone", "led", "camera","microscope","something_else", "trial_state_","encoder_val","angular_vel"] #made an array with header names
 rd= pd.read_csv('name=Calibration_data_st=M_sn=1trial=0.dat', names=hn,)
 rd['time'] = pd.to_datetime(rd['timestamp_arduino'])
-fig, ax1 = plt.subplots()
-color = 'red'
-ax1.set_xlabel('time')
-ax1.set_ylabel('angular velocity', color=color)
-ax1.plot(rd.time.dt.time, rd['angular_vel'], color=color)
-ax1.tick_params(axis='y', labelcolor=color)
-
-
 g=min(rd.angular_vel)
 tc=rd[rd.angular_vel==g].index.values[0]
-f=rd[rd.index < tc]
-
+h=rd[rd.index < tc]
+f=h[h.angular_vel < 0]
 fig, ax1 = plt.subplots()
 color = 'red'
 ax1.set_xlabel('time')
